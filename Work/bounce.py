21c70fbf3e34870081b78b7f1f132085e87d4c2f
Meera Sundar <mirage.sundial@gmail.com>
2020-11-06 15:25:52 +0530

    add sears and bounce programs

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+number = 0
+height = 100
+
+while number <= 10:
+    number = number + 1
+    height = height * ( 3 / 5)
+    print(number, end=' ')
+    print(round(height, 4))
